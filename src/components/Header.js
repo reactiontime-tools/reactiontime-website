e20d7cf928ded79758e34d05065fa3654d11aa80
jayacados <jayacados@gmail.com>
2020-05-22 01:58:10 -0700

    Initial commit, boiler plate code with file structure and partial setup of About page

---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-hourglass-end"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>ReactionTime</h1>
+        <p>
+          A simpler way to write tests for React's Experimental Concurrent Mode.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,204 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import pic01 from '../images/pic01.jpg'
+import pic02 from '../images/pic02.jpg'
+import pic03 from '../images/pic03.jpg'
+import jimmy from '../images/jimmy.jpg'
+import tony from '../images/tony.jpg'
+import christian from '../images/christian.jpg'
+import jae from '../images/jae.jpg'
+
+class Main extends React.Component {
+  render() {
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      ></div>
+    )
+
+    return (
+      <div
+        ref={this.props.setWrapperRef}
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="intro"
+          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+            }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Intro</h2>
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <p>
+            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
+            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
+            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
+            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
+            By the way, check out my <a href="#work">awesome work</a>.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
+            libero eu nibh porttitor fermentum. Nullam venenatis erat id
+            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
+            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
+            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
+            lectus. Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. In non lorem sit amet elit
+            placerat maximus. Pellentesque aliquam maximus risus, vel sed
+            vehicula.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="work"
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+            }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Work</h2>
+          <span className="image main">
+            <img src={pic02} alt="" />
+          </span>
+          <p>
+            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
+            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
+            urna nisi, fringila lorem et vehicula lacinia quam. Integer
+            sollicitudin mauris nec lorem luctus ultrices.
+          </p>
+          <p>
+            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
+            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
+            Pellentesque condimentum sem. In efficitur ligula tate urna.
+            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
+            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
+            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
+            tempus.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="about"
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+            }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">About</h2>
+          <span className="image minor"
+            style={{
+              textAlign: "center",
+              marginLeft: "33%"
+            }}
+          >
+            <img src={jimmy} alt="jimmy" />
+            <p>Jimmy Phong</p>
+            <img src={tony} alt="tony" />
+            <p>Jimmy Phong</p>
+            <img src={christian} alt="christian" />
+            <p>Jimmy Phong</p>
+            <img src={jae} alt="jae" />
+            <p>Jimmy Phong</p>
+          </span>
+          <p>
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
+            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
+            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
+            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
+            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
+            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
+            amet.
+          </p>
+          {close}
+        </article>
+
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+            }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Got a Question?</h2>
+          <h3 className="">Let us know below!</h3>
+          <br />
+          <form method="post" action="#">
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="4"></textarea>
+            </div>
+            <ul className="actions">
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
+            </ul>
+          </form>
+          <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://facebook.com" className="icon fa-facebook">
+                <span className="label">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com" className="icon fa-instagram">
+                <span className="label">Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/oslabs-beta/ReactionTime"
+                className="icon fa-github"
+              >
+                <span className="label">GitHub</span>
+              </a>
+            </li>
+          </ul>
+          {close}
+        </article>
+      </div>
+    )
+  }
+}
+
+Main.propTypes = {
+  route: PropTypes.object,
+  article: PropTypes.string,
+  articleTimeout: PropTypes.bool,
+  onCloseArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+  setWrapperRef: PropTypes.func.isRequired,
+}
+
+export default Main
