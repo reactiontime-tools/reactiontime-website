8ee5af28940c9e3748d08261e4332e7469f61e4b
Jimmy Phong <jayacados@gmail.com>
2020-05-23 12:25:44 -0700

    Updates

---
+++ component---src-pages-page-2-js-00c2c9c1b70f152df569.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),o=t.n(a),l=t("Wbzz"),c=t("Bl7J");n.default=function(){return o.a.createElement(c.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-00c2c9c1b70f152df569.js.map
